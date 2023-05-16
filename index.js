067da41fb953da0fd7542388fc7d1e1e6643212a
Shakil Ahmed <shakilahmedshuvo612@gmail.com>
2023-05-17 00:01:04 +0600

    add jwt access token function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
@@ -33,6 +34,18 @@ async function run() {
         const serviceCollection = client.db('carDoctor').collection('services');
         const bookingCollection = client.db('carDoctor').collection('bookings');
 
+        // jwt api
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            console.log(user);
+            // jwt function for expiration
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            // result send in a object {}
+            res.send({ token });
+        })
+
+
+        // services routes api
         app.get('/services', async (req, res) => {
             const cursor = serviceCollection.find();
             const result = await cursor.toArray();
